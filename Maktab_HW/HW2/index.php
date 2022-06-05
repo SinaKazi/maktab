2c207f3e65ee0ca9a2f224af6eedbd649a5e7211
SinaKazi <Dr.TinTin92@gmail.com>
2022-06-05 22:24:08 +0430

    t3

--- Maktab_HW/HW2/index.php
+++ Maktab_HW/HW2/index.php
@@ -1,2 +1,7 @@
 <?php
-$dna = "hygsafdsabdfcijgsad gsa";
+$humandna = "hygsafdsabdfcijgsad gsa";
+$humandna = explode(" ", $humandna);
+$dnah = strtoupper($humandna[0]);
+$dnav = strtoupper($humandna[1]);
+$searchv = preg_match_all('/(g)(s)(a)/i', $dnah ,$matches);
+print_r($matches);
