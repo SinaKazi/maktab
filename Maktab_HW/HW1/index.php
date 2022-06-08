5cd81e1baf2c7596544eaa1a7668119af0fd4a75
SinaKazi <Dr.TinTin92@gmail.com>
2022-06-08 13:34:01 +0430

    fin hw1

---
+++ Maktab_HW/HW1/index.php
@@ -0,0 +1,33 @@
+<?php
+//Q yakhdarchi
+
+$t = 110;
+
+if ($t > 100) {
+    echo "steam";
+} elseif ($t < 0) {
+    echo "ice";
+} else {
+    echo "water";
+}
+echo "<hr>";
+//Q Pythagoras
+
+$v = 5;
+$z1 = 4;
+$z2 = 3;
+
+if (pow($z1, 2) + pow($z2, 2) == pow($v, 2)) {
+    echo "yes";
+} else {
+    echo "no";
+}
+echo "<hr>";
+//Q Mobile
+
+$k = 80;
+
+for ($i = 0; $i < $k; $i++) {
+    $i += 2;
+}
+echo $i;
